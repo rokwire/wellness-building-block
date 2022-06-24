068b3006d33f1f0b7e95c7d61e00b2fb83eb00fe
Mladen Dryankov <mdryankov@gmail.com>
2022-06-24 15:32:05 +0300

    v1.0.1

--- docs/docs.go
+++ docs/docs.go
@@ -166,7 +166,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.2.0",
+	Version:          "1.0.1",
 	Host:             "localhost",
 	BasePath:         "/wellness",
 	Schemes:          []string{"https"},
